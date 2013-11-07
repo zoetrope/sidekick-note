d6a2aaeb9e478d2daf86e45c7bcfaa52f160f5f2
zoetrope <a.ikezoe@gmail.com>
2013-11-07 20:11:38 +0900

    ignore schema_version table (for flyway) when mysqldump

--- etc/backup.sh
+++ etc/backup.sh
@@ -3,6 +3,6 @@
 BACKUP_DIR=~/Dropbox/sidekick-note/data/
 TODAY=`date '+%Y%m%d%H%M'`
 
-mysqldump -u root -p sidekicknote | gzip > $TODAY.sql.gz
+mysqldump -u root -p --ignore-table=sidekicknote.schema_version sidekicknote | gzip > $TODAY.sql.gz
 #mysqldump -u root -p --no-create-info sidekicknote | gzip > $TODAY.sql.gz
 mv $TODAY.sql.gz $BACKUP_DIR
