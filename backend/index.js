9cd485972729f4780a38032fd7839f4dc28d26ab
zoetrope <a.ikezoe@gmail.com>
2014-02-03 22:57:49 +0900

    dummyのAPIを用意

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,30 @@
 var koa = require('koa');
 var app = koa();
+var router = require("koa-router");
+var logger = require("koa-logger");
+var mount = require("koa-mount");
+
+app.use(logger());
 
 var static = require('koa-static');
 app.use(static("client"));
-app.use(static("../public"));
+app.use(mount("/assets", static("../public")));
 
 app.use(require('./task').middleware());
+
+app.use(router(app));
+
+app.get("/api/login", function *(next){
+
+});
+
+app.get("/api/logout", function *(next){
+
+});
+
+app.get("/api/loggedin", function *(next){
+    this.body = {status: "OK"};
+});
+
 
 app.listen(3000);
