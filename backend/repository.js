e7465d5b47e4e9af6130f35fb3e7814b563714f3
zoetrope <a.ikezoe@gmail.com>
2014-02-11 13:26:51 +0900

    MongoDBへの接続URIを環境変数から取得

--- backend/repository.js
+++ backend/repository.js
@@ -1,8 +1,13 @@
 var mongojs = require('mongojs');
-var db = mongojs("sidekicknote", ["items", "criteria"]);
+// 環境変数からMongoDBへの接続文字列を取得。なければlocalhostに接続。
+var mongo_uri = process.env.MONGO_URI || "localhost/sidekicknote";
+console.log("connect to " + mongo_uri);
+var db = mongojs.connect(mongo_uri);
 
 var items = db.collection("items");
 var criteria = db.collection("criteria");
+var accounts = db.collection("accounts");
 
 exports.items = items;
 exports.criteria = criteria;
+exports.accounts = accounts;
